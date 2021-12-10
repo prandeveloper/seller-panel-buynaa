b9928237be49ebd6964e35948cb883fcfebfe5ad
prandeveloper <87694761+prandeveloper@users.noreply.github.com>
2021-12-10 10:22:36 +0530

    build

--- build/static/js/3.0c36a633.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[3],{849:function(a,s,d){}}]);
-//# sourceMappingURL=3.0c36a633.chunk.js.map
--- build/static/js/301.982b6f28.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[301],{2186:function(a,s){}}]);
-//# sourceMappingURL=301.982b6f28.chunk.js.map
